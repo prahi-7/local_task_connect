2f3e2849abf2b95c3e2c3526dd0490fd25ceb001
MasterPavanKumar <157820868+MasterPavanKumar@users.noreply.github.com>
2026-05-18 21:50:29 +0530

    fix cors and middleware

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,10 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin:"*"
+}));
+
 app.use(express.json());
 
 // ============ MONGODB ATLAS CONNECTION ============
